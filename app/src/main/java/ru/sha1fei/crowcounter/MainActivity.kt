1b6cf1d39b07a39ad2459ab36e0757d5175081c3
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2024-08-02 21:06:54 +0700

    add crow counter

---
+++ app/src/main/java/ru/sha1fei/crowcounter/MainActivity.kt
@@ -0,0 +1,33 @@
+package ru.sha1fei.crowcounter
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private var counter: Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.setText("Количество ворон: ${++counter}")
+        val button: Button = findViewById(R.id.button_counter)
+        button.setOnClickListener {v ->
+            textView.text = "Количество ворон: ${++counter}"
+            v.setBackgroundColor(if((counter %2) !=0) Color.RED else Color.BLUE )
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
